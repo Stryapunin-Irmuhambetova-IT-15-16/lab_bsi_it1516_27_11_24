d7398f783444917eb43290964bf40d7d5ff8e61b
Stryapunin-Irmuhambetova-IT-15-16 <strapuninirmuhambetova@gmail.com>
2024-11-27 14:29:15 +0500

    Основная функция - ввод числа
    Ирмухамбетова 14:28 27,11,24

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "Header.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+    int A, B, i;
+    cout << "Введите число ";
+    cin >> A; cout <<  endl;
+    B = randomnumber();
+    switch (proverka(A, B)) {
+        case 1: cout<< "Верно!"; break;
+        case 2: cout << "Не то число, нужное - больше "; break;
+        case 3: cout << "Не то число, нужное - меньше "; break;
+    }
+}
+
